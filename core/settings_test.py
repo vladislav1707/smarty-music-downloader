fd0e0c74f1bf332792f185ceb967fc13738e9089
Vladislav <pomidorchik427@gmail.com>
2026-08-12 17:45:25 +0300

    deleted settings_test.py and moved CLI.py 2

--- core/settings_test.py
+++
@@ -1,7 +0,0 @@
-from settings import Settings
-
-s = Settings()
-s.reset_all
-print("max_working_proxies:", s.get("max_working_proxies"))
-print("proxy_provider:", s.get("proxy_provider"))
-print("profiles_dir:", s.get("profiles_dir"))
