4114ff73567f88ab2839cac7b9a5fe9268f595c3
Vladislav <pomidorchik427@gmail.com>
2026-08-17 18:36:22 +0300

    proxy_rotator.py added

--- core/proxy_rotator.py
+++ core/proxy_rotator.py
@@ -1,6 +1,41 @@
+import time
+import logging
 from proxy_manager import ProxyManager
 from settings import Settings
 
+# create a logger with the same name as the file (proxy_rotator)
+logger = logging.getLogger(__name__)
+
 class ProxyRotator:
     def __init__(self, settings: Settings):
+        # settings
         self._settings = settings
+        self._refresh_interval = self._settings.get("proxy_refresh_interval")
+        # proxy_manager
+        self._proxy_manager = ProxyManager(self._settings)
+
+        self._last_update = 0.0
+        self._proxy_list = []
+        self._current_proxy = None
+
+    # проверка на то что требуется обновление
+    def _need_refresh(self) -> bool:
+        return (time.monotonic() - self._last_update) >= self._refresh_interval
+
+    # если требуется обновление то обновить список прокси
+    def _update_proxy(self):
+        if self._need_refresh() or self._current_proxy == None:
+            self._proxy_list = self._proxy_manager.fetch_proxies()
+            self._last_update = time.monotonic()
+
+    # сменить прокси на следующий
+    def next_proxy(self):
+        self._update_proxy()
+        # выбрать последний элемент списка прокси в качестве текущего прокси а так же удалить его
+        if self._proxy_list != []:
+            self._current_proxy = self._proxy_list.pop()
+    
+    # получить текущий прокси
+    def get_proxy(self):
+        self._update_proxy()
+        return self._current_proxy
--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,8 @@ class Settings:
     DEFAULTS = {
     "max_working_proxies": 5,
     "proxy_url": "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/protocols/socks5/data.txt",
-    "profiles_dir": "profile_presets" # "profile_presets" is a special value that points to presets
+    "profiles_dir": "profile_presets", # "profile_presets" is a special value that points to presets
+    "proxy_refresh_interval": 305
     }
 
     # Settings class constructor
